2834a783ac4518593ce09a2de7963169d2e2c9f4
psuwala <psuwala@atlassian.com>
2018-08-15 22:00:37 +0200

    Shows image (?)

--- src/main/java/com/psuwala/image/util/ShowUtil.java
+++ src/main/java/com/psuwala/image/util/ShowUtil.java
@@ -1,6 +1,8 @@
 package com.psuwala.image.util;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * TODO: Document this class / interface here
@@ -9,6 +11,22 @@ import javax.swing.*;
  */
 public class ShowUtil {
 
-    static JFrame showImage(){}
+    public static JFrame showImage(BufferedImage image) {
+        JFrame jFrame = new JFrame("marzanna") {
+            @Override
+            public void paint(Graphics g) {
+                super.paintComponents(g);
+                g.setColor(Color.red);
+                g.fillRect(0, 0, 200, 200);
+                g.drawImage(image, 10, 10, null);
+            }
+        };
+
+        jFrame.setSize(image.getWidth() + 200, image.getHeight() + 200);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        return jFrame;
+    }
 
 }
--- src/test/java/com/psuwala/image/AppTest.java
+++ src/test/java/com/psuwala/image/AppTest.java
@@ -1,20 +1,26 @@
 package com.psuwala.image;
 
-import static org.junit.Assert.assertTrue;
-
+import com.psuwala.image.util.ShowUtil;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.IOException;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @Ignore
+    public void showTestImagexD() throws IOException, InterruptedException {
+        JFrame jFrame = ShowUtil.showImage(ImageIO.read(AppTest.class.getResourceAsStream("/002112.bmp")));
+        while(jFrame.isVisible()){
+            Thread.sleep(100);
+        }
     }
 }
